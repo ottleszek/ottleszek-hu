08dcd5c859e030276bf6ea6593840c68e591ad8f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-07-31 10:02:06 +0200

    Mobil projekt létrejött

---
+++ WillBeThere.Mobile/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace WillBeThere.Mobile
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
