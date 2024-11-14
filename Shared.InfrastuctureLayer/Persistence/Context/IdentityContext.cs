e61d389f149c1b130165207af9a8a6a3853f05d3
gycsaba <gycsaba@SZT9-24>
2024-11-14 15:49:57 +0100

    User, IdentityContext

--- Shared.InfrastuctureLayer/Persistence/Context/IdentityContext.cs
+++ Shared.InfrastuctureLayer/Persistence/Context/IdentityContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Shared.InfrastuctureLayer.Modules.Authentication.Models;
 
 namespace Shared.InfrastuctureLayer.Persistence.Context
 {
     public class IdentityContext : IdentityDbContext<User>
     {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+        }
     }
 }
