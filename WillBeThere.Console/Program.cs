dba558a1ccc71a0cf872bbf4d3ca34378397f55e
gycsaba <gycsaba@SZT9-24>
2024-11-21 16:00:18 +0100

    Eamil alapú regisztráció működik

--- WillBeThere.Console/Program.cs
+++ WillBeThere.Console/Program.cs
@@ -1,28 +1,28 @@
 ﻿using MailKit.Net.Smtp;
 using MimeKit;
-using System.Net;
 
 try
 {
     // https://www.gmass.co/smtp-test
-    using var email = new MimeMessage();
-    email.From.Add(new MailboxAddress("Sender name", "from@example.com"));
-    email.To.Add(new MailboxAddress("Gyuris Csaba", "gycsaba72@gmail.com"));
+    var newemial = new MimeMessage();
+    newemial.From.Add(new MailboxAddress("Sender name", "from@example.com"));
+    newemial.To.Add(new MailboxAddress("Gyuris Csaba", "gycsaba72@gmail.com"));
 
-    email.Subject = "próba";
+    newemial.Subject = "próba";
 
     var builder = new BodyBuilder()
     {
         TextBody = "Szöveg"
     };
 
-    email.Body=builder.ToMessageBody();
+    newemial.Body=builder.ToMessageBody();
 
     using var smtp = new SmtpClient();
     Console.WriteLine(smtp);
-    smtp.Connect(host: "sandbox.smtp.mailtrap.io", port: 587,useSsl:false);
+    //smtp.Connect(host: "sandbox.smtp.mailtrap.io", port: 465,useSsl:false);
+    smtp.Connect("sandbox.smtp.mailtrap.io", 465, MailKit.Security.SecureSocketOptions.StartTls);
     smtp.Authenticate("993bd8fcb037f9", "8e3d7fb1e7bdbd");
-    await smtp.SendAsync(email);
+    await smtp.SendAsync(newemial);
     smtp.Disconnect(true);
     Console.WriteLine("Done");
 }
--- WillBeThere.InfrastuctureLayer/Email/UserEmailSender.cs
+++ WillBeThere.InfrastuctureLayer/Email/UserEmailSender.cs
@@ -1,13 +1,41 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using MimeKit;
+using MailKit.Net.Smtp;
 using Shared.InfrastuctureLayer.Modules.Authentication.Models;
+
 
 namespace WillBeThere.InfrastuctureLayer.Email
 {
     public class UserEmailSender : IEmailSender<User>
     {
-        public Task SendConfirmationLinkAsync(User user, string email, string confirmationLink)
+        public async Task SendConfirmationLinkAsync(User user, string email, string confirmationLink)
         {
-            throw new NotImplementedException();
+            using var newemail = new MimeMessage();
+            newemail.From.Add(new MailboxAddress("Regisztráció", "noreplay@ottleszek.hu"));
+            newemail.To.Add(new MailboxAddress(user.Email, user.Email));
+
+            var builder = new BodyBuilder();
+            builder.HtmlBody = $@"
+                    <html>
+                        <body>
+                            <h2>Kedves {user.Email}!</h2>
+                            <p>Köszönjük, hogy regisztráltál! Kérjük, erősítsd meg a regisztrációdat a következő linkre kattintva:</p>
+                            <p><a href='{confirmationLink}'>Regisztráció megerősítése</a></p>
+                            <p>Üdvözlettel,<br>Az Ottleszek.hu csapata</p>
+                        </body>
+                    </html>";
+
+            builder.TextBody = $"Kedves {user.Email}!\n\nKérjük, erősítsd meg a regisztrációdat az alábbi linkre kattintva:\n{confirmationLink}\n\nÜdvözlettel,\nAz Ottleszek.hu csapata";
+
+            newemail.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            Console.WriteLine(smtp);
+            //smtp.Connect(host: "sandbox.smtp.mailtrap.io", port: 465,useSsl:false);
+            smtp.Connect("sandbox.smtp.mailtrap.io", 465, MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate("993bd8fcb037f9", "8e3d7fb1e7bdbd");
+            await smtp.SendAsync(newemail);
+            smtp.Disconnect(true);
+
         }
 
         public Task SendPasswordResetCodeAsync(User user, string email, string resetCode)
